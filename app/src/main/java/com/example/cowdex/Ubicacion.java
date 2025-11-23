1a951bd7c8e3998e3dd1f9d5b026f71cf22c7e89
Angel <220665@utags.edu.mx>
2025-11-23 16:37:23 -0600

    Corte antes de implementar lo de APIS

---
+++ app/src/main/java/com/example/cowdex/Ubicacion.java
@@ -0,0 +1,36 @@
+package com.example.cowdex;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Ubicacion extends Fragment {
+
+    private UbicacionViewModel mViewModel;
+
+    public static Ubicacion newInstance() {
+        return new Ubicacion();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ubicacion, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(UbicacionViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/cowdex/UbicacionViewModel.java
@@ -0,0 +1,7 @@
+package com.example.cowdex;
+
+import androidx.lifecycle.ViewModel;
+
+public class UbicacionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
